716a4408bb90b7511c538999b4c50f9bfb6fe3aa
k-fish <fisherk.work@gmail.com>
2019-10-11 15:28:48 -0400

    Add evented to service

--- addon/services/user-performance-monitoring.js
+++ addon/services/user-performance-monitoring.js
@@ -1,22 +1,23 @@
 import Service from '@ember/service';
+import Evented from '@ember/object/evented';
 
-export default Service.extend({
-  init() {
+export default Service.extend(Evented, {
+  _onEvent(eventName, eventDetails) {
+    this.trigger('timingEvent', eventName, eventDetails);
+  },
+
+  listen() {
     const callbackClosure = (eventName, e) => {
       this._onEvent(eventName, e);
     };
-    if (window.__emberUserPerf) {
+
+    if (window.__emberUserPerf && !window.__emberUserPerfCallback) {
       window.__emberUserPerfCallback = callbackClosure;
 
       Object.entries(window.__emberUserPerf).forEach(([key, value]) => {
         callbackClosure(key, value);
       });
     }
-  },
-
-  _onEvent(eventName, e) {
-    if (this.onEvent) {
-      this.onEvent(eventName, e);
-    }
   }
 });
+
